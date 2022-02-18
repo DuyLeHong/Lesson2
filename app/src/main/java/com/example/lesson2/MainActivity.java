e231f51579d92e921114e61d30732863f5e48178
Duy Le <lehongduy.bk@gmail.com>
2022-02-18 19:49:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lesson2/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.lesson2;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        UCrop.of(sourceUri, destinationUri)
+//                .withAspectRatio(16, 9)
+//                .withMaxResultSize(maxWidth, maxHeight)
+//                .start(context);
+
+        CropImage.activity()
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .start(this);
+
+    }
+}
